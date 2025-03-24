9d907dd96997d62edd8a4b8d9f23b95d50847faa
VidaTheHydroxZ <david.vidakovic01@gmail.com>
2025-03-24 23:45:19 +0100

    Refactoring code! Removed players and ball as pointer, unnecessary heap memory allocation!

--- src/input.c
+++ src/input.c
@@ -38,19 +38,19 @@ void process_input()
     {
         game_is_running = 0;
     }
-    if (state[SDL_SCANCODE_W] && inputEntity->player1->rectangle->y > 0)
+    if (state[SDL_SCANCODE_W] && inputEntity->player1.rectangle->y > 0)
     {
         move_player_one_up();
     }
-    if (state[SDL_SCANCODE_S] && (inputEntity->player1->rectangle->y + inputEntity->player1->rectangle->h) < WINDOW_HEIGHT)
+    if (state[SDL_SCANCODE_S] && (inputEntity->player1.rectangle->y + inputEntity->player1.rectangle->h) < WINDOW_HEIGHT)
     {
         move_player_one_down();
     }
-    if (state[SDL_SCANCODE_O] && inputEntity->player2->rectangle->y > 0)
+    if (state[SDL_SCANCODE_O] && inputEntity->player2.rectangle->y > 0)
     {
         move_player_two_up();
     }
-    if (state[SDL_SCANCODE_L] && (inputEntity->player2->rectangle->y + inputEntity->player2->rectangle->h) < WINDOW_HEIGHT)
+    if (state[SDL_SCANCODE_L] && (inputEntity->player2.rectangle->y + inputEntity->player2.rectangle->h) < WINDOW_HEIGHT)
     {
         move_player_two_down();
     }
